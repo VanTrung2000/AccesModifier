060fe19243ddcc8e5a30502e9aef940d23dbbac2
VanTrung2000 <vantrungdeptrai2000@gmail.com>
2020-09-21 11:42:38 +0700

    Add project files.

---
+++ Acessmodifer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Acessmodifer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle ci1 = new Circle();
+            ci1.setColor("red");
+            ci1.setRadius(2.0);
+            Console.WriteLine(ci1.getColor());
+            Console.WriteLine(ci1.getRadius());
+        }
+    }
+}
